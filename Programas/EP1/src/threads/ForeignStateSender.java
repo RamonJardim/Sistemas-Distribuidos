daa63e4de14497a64dd340dd7ac6bb563ea64d42
Ramon <ramon-neres@hotmail.com>
2019-07-09 14:49:24 -0300

    Remocao de warning e linha para debug

--- Programas/EP1/src/threads/ForeignStateSender.java
+++ Programas/EP1/src/threads/ForeignStateSender.java
@@ -31,6 +31,7 @@ public class ForeignStateSender extends Thread { // Este representa T3
                 Thread.sleep(500);
                 infoPeerToSendName = Randomizer.getRandomPeer(DAO.getPeerName());
                 infoToSend = DAO.getDAO().getForeignInfo(infoPeerToSendName);
+                System.out.println("Enviar para: " + infoToSend);
             } while(infoToSend == null);
 
             infoToSend.setPeerSender(DAO.getPeerName());
--- Programas/EP1/src/threads/OldStateCleaner.java
+++ Programas/EP1/src/threads/OldStateCleaner.java
@@ -14,7 +14,7 @@ public class OldStateCleaner extends Thread { //T4
     private void cleanInfo() {
         while (true) {
             Map<String, PeerInfo> foreignersInfo = DAO.getDAO().getForeignsMap();
-            foreignersInfo.forEach((k, v) -> {
+            foreignersInfo.forEach((String k, PeerInfo v) -> {
                 if (System.currentTimeMillis() - foreignersInfo.get(k).getReceiveMoment() > 1500 && !k.equals(DAO.getPeerName())) {
                     System.out.println(String.format("Console peer %s: Thread T4 – eliminando estado do peer %s", DAO.getPeerName(), k));
                     DAO.getDAO().removePeerInfo(k);
