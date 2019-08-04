425bd83758f02e1ed25eea31047b9ae3d6a74679
Ramon <ramon-neres@hotmail.com>
2019-08-04 16:57:58 -0300

    Alteração nos IPs

--- Programas/EP2/Peer/src/data/Neighbors.java
+++ Programas/EP2/Peer/src/data/Neighbors.java
@@ -4,16 +4,16 @@ import java.util.HashMap;
 
 public abstract class Neighbors {
     public static final HashMap<String, String> NEIGHBORS = new HashMap<String, String>() {{
-        put("A", "54.208.74.87");
-        put("B", "54.158.237.235");
-        put("C", "54.89.99.34");
-        put("D", "3.82.246.240");
-        put("E", "3.86.35.100");
-        put("F", "3.84.251.158");
-        put("G", "54.82.44.133");
-        put("H", "18.207.187.65");
-        put("I", "3.83.158.220");
-        put("J", "3.90.11.149");
+        put("A", "3.91.181.190");
+        put("B", "34.227.25.157");
+        put("C", "34.207.160.141");
+        put("D", "34.201.67.183");
+        put("E", "52.91.172.144");
+        put("F", "3.83.91.22");
+        put("G", "52.206.228.157");
+        put("H", "34.205.146.94");
+        put("I", "18.205.27.112");
+        put("J", "18.208.165.130");
     }};
     public static final int STATE_EXCHANGE_PORT = 9876;
     public static final int DOWNLOAD_PORT = 8765;
